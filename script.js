9bf4cce65761559077097d310acfb6b3bcdb577f
bwh <113823007+boywithalo@users.noreply.github.com>
2023-04-14 20:50:06 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      document.getElementById("preloader").style.opacity = "0"; // Set opacity to 0
+    }, 400); // 1000ms = 1 second delay
+  });
